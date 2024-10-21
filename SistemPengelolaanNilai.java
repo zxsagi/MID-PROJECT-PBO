cb2d2d8bf275dfbaffcf076c6c728918eda17709
fridemmulli <2382018@unai.edu>
2024-10-21 10:44:34 +0700

    Create SistemPengelolaanNilai.java

---
+++ SistemPengelolaanNilai.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Siswa {
+  private String nama;
+  private String id;
+  private List<Double> nilai;
+
+  public Siswa(String nama,String id}){
+  this.nama = nama;
+    this.id = id;
+    this.nilai = new ArrayList<>0;
+}
+  public String getNama(){
+    return nama;
+  }
+  public String getld(){
+    return id;
+  }
+  public void tambahNIlai(doublenilai){
+    this.nilai.add(nilai);
+  }
+  public List<Double> getNilai(){
+    return nilai;
+  }
+}
+  
