b8d52971ba699631de012303cdcda79809e52610
fridemmulli <2382018@unai.edu>
2024-10-21 20:19:36 +0700

    Update SistemPengelolaanNilai.java

--- SistemPengelolaanNilai.java
+++ SistemPengelolaanNilai.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+//Kelas Siswa- Anggota 3; Mengelola data siswa dan menampilkan grafik
 class Siswa {
   private String nama;
   private String id;
@@ -23,4 +24,35 @@ class Siswa {
     return nilai;
   }
 }
+
+//Kelas PengelolaSiswa - Anggota 3; Mengelola siswa untuk grafik 
+class Pengelola Siswa{
+  private List<siswa> = new
+  ArrayList<>0;
+}
+
+ public void tambahSiswa(Siswa siswa) {
+        siswaList.add(siswa);
+    }
+
+    public List<Siswa> getSiswaList() {
+        return siswaList;
+    }
+}
+// Kelas GrafikPerkembangan - Anggota 3: Tampilkan grafik dan notifikasi
+class GrafikPerkembangan {
+    // Anggota 3: Tampilkan grafik perkembangan nilai siswa
+    public void tampilkanGrafik(List<Siswa> siswaList) {
+        // Implementasi untuk menampilkan grafik perkembangan nilai siswa
+        System.out.println("Grafik perkembangan nilai siswa (fungsi belum diimplementasikan).");
+    }
+
+    // Anggota 3: Notifikasi perkembangan siswa
+    public void notifikasiPerkembangan(Siswa siswa) {
+        // Implementasi untuk memberikan notifikasi perkembangan
+        System.out.println("Notifikasi perkembangan untuk " + siswa.getNama() + " (fungsi belum diimplementasikan).");
+    }
+}
+
+
   
